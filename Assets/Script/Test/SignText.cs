5e7a7a5a349422b420e95112ef40587ddc3d84e6
JeffJi <jjc1114@sina.com>
2021-02-08 21:45:22 +0800

    text1

---
+++ Assets/Script/Test/SignText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignText : MonoBehaviour
+{
+
+    public Transform textPosition;
+    public Text textUI;
+
+    private void Update()
+    {
+        textUI.transform.position = textPosition.position;
+    }
+
+    public void KillText()
+    {
+        textUI.text = "E to Kill";
+    }
+}
