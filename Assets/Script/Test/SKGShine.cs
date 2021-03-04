1b981fa2c6dec870f8109f6ae6af099b21a99c7a
GDLAB-MSI09\GD Student <zhangyijie16@163.com>
2021-03-04 11:48:24 +0800

    update

---
+++ Assets/Script/Test/SKGShine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SKGShine : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public ItemMoveable card;
+    private bool flag;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        flag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(card!=null)
+        {
+            if (card.startDetect == true && flag == false)
+            {
+                StartCoroutine(Shine());
+            }
+        }
+    }
+
+    IEnumerator Shine()
+    {
+        flag = true;
+        sr.enabled = true;
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(StopShine());
+    }
+
+    IEnumerator StopShine()
+    {
+        sr.enabled = false;
+        yield return new WaitForSeconds(1f);
+        flag = false;
+    }
+}
